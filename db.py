976afc42432e0f7576d5c2766ad393a64bcfbb62
Caio Almeida <mcalm.139@gmail.com>
2025-06-19 15:36:27 -0300

    DataBase Connection

---
+++ db.py
@@ -0,0 +1,16 @@
+import psycopg2
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+db_password = os.getenv("DB_PASSWORD")
+
+
+conn = psycopg2.connect(
+    
+    host = "localhost",
+    port = 5432,
+    database = "mydb",
+    user = "postgres",
+    password = db_password
+)
