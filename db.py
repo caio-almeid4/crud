040a1c5edca91b4106f8f51aca1d4dbf85d75bb5
Caio Almeida <mcalm.139@gmail.com>
2025-06-24 18:39:43 -0300

    remove redundance

--- db.py
+++ db.py
@@ -5,14 +5,6 @@ import os
 load_dotenv()
 db_password = os.getenv("DB_PASSWORD")
 
-conn = psycopg2.connect(
-    host = "localhost",
-    port = 5432,
-    database = "mydb",
-    user = "postgres",
-    password = db_password
-)
-
 def connect_db():
     
     conn = psycopg2.connect(
