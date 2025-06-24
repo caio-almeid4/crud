a4e01de06dedf2e58819eabb2485f5eb4bee25bd
Caio Almeida <mcalm.139@gmail.com>
2025-06-24 17:45:24 -0300

    deploy delete and get user with fastapi

--- crud.py
+++ crud.py
@@ -1,49 +1,51 @@
-from fastapi import FastAPI
 from db import connect_db
 
-
-app = FastAPI()
-
-class User:
-    
-    def __init__(
-        self,
-        name: str,
-        age: int,
-        email: str
-    ) -> None:
-        
-        
-        self.name = name
-        self.age = age
-        self.email = email
            
-    def create(self):
+def create(name: str, age: int, email: str):
         
-        conn = connect_db()
-        cur = conn.cursor()
-        
-        cur.execute(
-            "INSERT INTO users (name, age, email) VALUES (%s, %s, %s)",
-            (self.name, self.age, self.email)
-        )
-        
-        conn.commit()
-        cur.close()
-        conn.close()
-        
-    def delete(self):
-        
-        conn = connect_db()
-        cur = conn.cursor()
-        
-        cur.execute(
-            "DELETE FROM users WHERE email = (%s)",
-            (self.email,)
-        )
-        
-        conn.commit()
-        cur.close()
-        conn.close()
+    conn = connect_db()
+    cur = conn.cursor()
     
+    cur.execute(
+        "INSERT INTO users (name, age, email) VALUES (%s, %s, %s)",
+        (name, age, email)
+    )
+    
+    conn.commit()
+    cur.close()
+    conn.close()
         
+def delete(email: str):
+        
+    conn = connect_db()
+    cur = conn.cursor()
+    
+    cur.execute(
+        "DELETE FROM users WHERE email = (%s)",
+        (email,)
+    )
+    
+    conn.commit()
+    cur.close()
+    conn.close()
+    
+    
+def  get(email: str):
+    
+    conn = connect_db()
+    cur = conn.cursor()
+    
+    cur.execute(
+        "SELECT name, age, email FROM users WHERE email = (%s)",
+        (email,)
+    )
+    
+    info = dict(zip(("name", "age", "email"), cur.fetchall()[0]))
+    
+    conn.commit()
+    cur.close()
+    conn.close()
+    
+    return info
+
+    
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from crud import User
 from fastapi import FastAPI
 from pydantic import BaseModel
+from crud import create, delete, get
 
 app = FastAPI()
 
@@ -11,14 +11,26 @@ class UserCreate(BaseModel):
     
 @app.post("/add_user")
 def create_user(user: UserCreate):
-    user_obj = User(user.name, user.age, user.email)
-    user_obj.create()
     
-    return {"message: user created successfully"}
+    create(user.name, user.age, user.email)
+   
+    
+    return {"message": "user created successfully"}
 
+@app.delete("/del_user")
+def delete_user(email: str):
     
+    delete(email)
     
+    return {"message": "user deleted successfully"}
     
+
+@app.get("/get_user")
+def get_user(email: str):
+    
+    return get(email)
+    
+
         
         
     
